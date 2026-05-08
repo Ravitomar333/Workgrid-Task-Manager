56fa27ce637c6a0eb1fec362a2408b742a420950
RaviTomar <ravi2310010d@gmail.com>
2026-05-08 12:55:19 +0530

    change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
   server: {
-    host: "0.0.0.0",
     port: 5173,
-  },
-
-  preview: {
-    host: "0.0.0.0",
-    port: process.env.PORT || 4173,
-    allowedHosts: true,
   },
 });
